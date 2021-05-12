65c35e5e60ac75c8d19158927dc0cd086eedfaab
Mujajahida <mujahidaafridi0307@gmail.com>
2021-05-12 18:56:08 +0500

    work

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(app());
+}
+
+class app extends StatelessWidget{
+@override
+Widget build(BuildContext context){
+
+  return MaterialApp(
+   home : Scaffold(
+     body: Text ("Mujahida mujahidaafridi0307@gmail.com"),
+   )
+  );
+}
+
+}
